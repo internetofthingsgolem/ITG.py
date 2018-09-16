f12051a73b455898054404c9f9e4d951d539a64d
ITG <42692768+internetofthingsgolem@users.noreply.github.com>
2018-09-16 15:33:42 +0800

    pass_tx test

---
+++ api.py
@@ -0,0 +1,68 @@
+import json
+import urllib2
+import sys
+
+#Utils:
+all_nines = '9' * 81
+TIMEOUT = 60
+def API(request,url=url):
+
+    stringified = json.dumps(request)
+    headers = {'content-type': 'application/json', 'X-IIG-API-Version': '1'}
+
+    try:
+        request = urllib2.Request(url=url, data=stringified, headers=headers)
+        returnData = urllib2.urlopen(request, timeout=TIMEOUT).read()
+        response = json.loads(returnData)
+
+    except:
+        print url, "Timeout!"
+        print '\n    ' + repr(sys.exc_info())
+        return ""
+    if not response:
+        response = ""
+    return response
+
+def getNodeInfo(url):
+    cmd = {
+        "command": "getNodeInfo"
+    }
+    return API(cmd, url)
+
+def getTrytes(url, hash):
+    cmd = {
+        "command": "getTrytes",
+        "hashes" : [hash]
+    }
+    return API(cmd, url)
+
+def attachToTangle(url, trunk_tx, branch_tx, mwm, unattached_trytes):
+    cmd = {
+        "command": "attachToTangle",
+        "trunkTransaction": trunk_tx,
+        "branchTransaction": branch_tx,
+        "minWeightMagnitude": mwm,
+        "trytes": [unattached_trytes]
+    }
+    return API(cmd, url)
+
+def storeTransactions(url, attached_trytes):
+    cmd = {
+        "command": "storeTransactions",
+        "trytes": [attached_trytes]
+    }
+    return API(cmd, url)
+
+def broadcastTransactions(url, attached_trytes):
+    cmd = {
+        "command": "broadcastTransactions",
+        "trytes": [attached_trytes]
+    }
+    return API(cmd, url)
+
+def findTransactions(url, tags):
+    cmd = {
+        "command": "findTransactions",
+        "tags": [tags]
+    }
+    return API(cmd, url)
